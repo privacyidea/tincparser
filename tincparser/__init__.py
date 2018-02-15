5277e28131959420ebb5131fa5a417aa0685b641
Friedrich Weber <friedrich.weber@netknights.it>
2018-02-15 15:21:52 +0100

    Add SFTP IO Handler

--- tincparser/__init__.py
+++ tincparser/__init__.py
@@ -1,4 +1,4 @@
 from .conf import TincConfFile
-from .io import LocalIOHandler
+from .io import LocalIOHandler, SFTPIOHandler
 
-__all__ = ['TincConfFile', 'LocalIOHandler']
+__all__ = ['TincConfFile', 'LocalIOHandler', 'SFTPIOHandler']
--- tincparser/io.py
+++ tincparser/io.py
@@ -15,9 +15,29 @@ class IOHandler(object):
     def exists(self, filename):
         raise NotImplementedError()
 
+
 class LocalIOHandler(IOHandler):
     def open(self, filename, mode):
         return open(filename, mode)
 
     def exists(self, filename):
         return os.path.exists(filename)
+
+
+class SFTPIOHandler(IOHandler):
+    """
+    IO handler that works remotely via paramiko+SFTP
+    """
+    def __init__(self, sftp):
+        IOHandler.__init__(self)
+        self.sftp = sftp
+
+    def open(self, filename, mode):
+        return self.sftp.file(filename, mode)
+
+    def exists(self, filename):
+        try:
+            self.sftp.stat(filename)
+            return True
+        except IOError:
+            return False
