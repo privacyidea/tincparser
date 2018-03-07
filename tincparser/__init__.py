a99df6745e011acda8818eb7fedb609580478f11
Friedrich Weber <friedrich.weber@netknights.it>
2018-03-07 13:59:24 +0100

    Add parser for nets.boot

--- tincparser/__init__.py
+++ tincparser/__init__.py
@@ -1,5 +1,6 @@
 from .conf import TincConfFile
 from .io import LocalIOHandler, SFTPIOHandler
 from .tinc_up import UpScript
+from .nets_boot import NetsBoot
 
-__all__ = ['TincConfFile', 'LocalIOHandler', 'SFTPIOHandler', 'UpScript']
+__all__ = ['TincConfFile', 'LocalIOHandler', 'SFTPIOHandler', 'UpScript', 'NetsBoot']
