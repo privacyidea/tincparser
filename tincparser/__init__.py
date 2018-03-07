3ce486ac3815a1eef75d16a57c8403eb12407be5
Friedrich Weber <friedrich.weber@netknights.it>
2018-03-07 13:29:36 +0100

    Add parser/generator for tinc-up scripts

--- tincparser/__init__.py
+++ tincparser/__init__.py
@@ -1,4 +1,5 @@
 from .conf import TincConfFile
 from .io import LocalIOHandler, SFTPIOHandler
+from .tinc_up import UpScript
 
-__all__ = ['TincConfFile', 'LocalIOHandler', 'SFTPIOHandler']
+__all__ = ['TincConfFile', 'LocalIOHandler', 'SFTPIOHandler', 'UpScript']
